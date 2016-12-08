226321e778546a5a713074301a8b347a6b6bad60
Jacob Gersztyn <jabocg@gmail.com>
2016-12-07 17:52:36 -0700

    Update ignore, unzip file
    Pretty close to completion. At least the easy part.

---
+++ slackdown.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: <encoding name> -*-
+
+"""
+Slackdown -- The Slack to Markdown converter.
+"""
+
+import zipfile
+import sys
+
+
+def __main__():
+    print(sys.argv)
+    zfile = zipfile.ZipFile(sys.argv[1])
+    zfile.extractall(path='.tmp/')
+
+
+if __name__ == '__main__':
+    __main__()
